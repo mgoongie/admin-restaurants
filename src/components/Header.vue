e274e1ca0abeac90922ec5e5dc2982624762037a
gammat <t.gasparian196@gmail.com>
2019-06-28 18:25:19 +0400

    project setups

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-toolbar>
+        <v-spacer></v-spacer><v-spacer></v-spacer><v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat icon><v-icon>search</v-icon></v-btn>
+            <v-btn flat icon><v-icon>settings</v-icon></v-btn>
+            <v-btn flat icon><v-icon>account_box</v-icon></v-btn>
+            <v-btn flat icon><v-icon>language</v-icon></v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+<script>
+    export default {
+        data: () => ({
+
+        }),
+        name: 'Toolbar',
+        updated() {
+
+        },
+        mounted() {
+
+        },
+        methods: {}
+    }
+</script>
